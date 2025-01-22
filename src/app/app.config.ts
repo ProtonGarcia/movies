1f3caa88a336364ef4b6d032aa7fa9ae76fa889e
ProtonGarcia <alexgarcia7210@gmail.com>
2025-01-22 00:52:48 -0600

    updaing version

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,2 @@
 import { ApplicationConfig } from '@angular/core';
 import { providePrimeNG } from 'primeng/config';
-import  Aura  from '@primeng/themes/aura'
-
-
-export const appConfig: ApplicationConfig = {
-    providers: [
-        providePrimeNG({ theme:{
-            preset: Aura
-        } }),
-
-    ]
-};
